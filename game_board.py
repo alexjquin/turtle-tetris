f4ccc4331ec37275efab629d453fb36e91a5cb7a
Alex Quin <alex.j.quin@outlook.com>
2022-10-10 15:26:03 -0700

    Redefine wall boundaries

--- game_board.py
+++ game_board.py
@@ -1,66 +1,64 @@
 from turtle import Turtle
 
 
+HEIGHT = 20
+WIDTH = 10
+
 class GameBoard:
     def __init__(self, square_size):
         self.square_size = square_size
-
-        self.LEFT_WALL = -5 * square_size
-        self.RIGHT_WALL = 5 * square_size
-        self.TOP_WALL = 10 * square_size
-        self.BOTTOM_WALL = -10 * square_size
 
         self.wall_turtle = Turtle()
         self.wall_turtle.hideturtle()
         self.wall_turtle.color("black")
         self.wall_turtle.pensize(5)
 
-        self.draw_outer_wall(square_size)
+        self.draw_outer_wall()
 
-        self.draw_columns(square_size)
+        self.draw_columns()
 
-        self.draw_rows(square_size)
+        self.draw_rows()
 
-    def draw_rows(self, square_size):
+    def draw_rows(self):
         self.wall_turtle.penup()
-        self.wall_turtle.goto(self.LEFT_WALL, self.TOP_WALL)
+        self.wall_turtle.goto(-WIDTH / 2 * self.square_size, HEIGHT / 2 * self.square_size)
         self.wall_turtle.pendown()
 
         for index in range(0, 10):
             self.wall_turtle.setheading(270)
-            self.wall_turtle.forward(square_size)
+            self.wall_turtle.forward(self.square_size)
             self.wall_turtle.setheading(0)
-            self.wall_turtle.forward(10 * square_size)
+            self.wall_turtle.forward(WIDTH * self.square_size)
             self.wall_turtle.setheading(270)
-            self.wall_turtle.forward(square_size)
+            self.wall_turtle.forward(self.square_size)
             self.wall_turtle.setheading(180)
-            self.wall_turtle.forward(10 * square_size)
+            self.wall_turtle.forward(WIDTH * self.square_size)
 
-    def draw_columns(self, square_size):
+    def draw_columns(self):
         self.wall_turtle.penup()
-        self.wall_turtle.goto(self.LEFT_WALL, self.TOP_WALL)
+        self.wall_turtle.goto(-WIDTH / 2 * self.square_size, HEIGHT / 2 * self.square_size)
         self.wall_turtle.pendown()
 
         for index in range(0, 5):
-            self.wall_turtle.forward(square_size)
+            self.wall_turtle.forward(self.square_size)
             self.wall_turtle.setheading(270)
-            self.wall_turtle.forward(20 * square_size)
+            self.wall_turtle.forward(HEIGHT * self.square_size)
             self.wall_turtle.setheading(0)
-            self.wall_turtle.forward(square_size)
+            self.wall_turtle.forward(self.square_size)
             self.wall_turtle.setheading(90)
-            self.wall_turtle.forward(20 * square_size)
+            self.wall_turtle.forward(HEIGHT * self.square_size)
             self.wall_turtle.setheading(0)
 
-    def draw_outer_wall(self, square_size):
+    def draw_outer_wall(self):
         self.wall_turtle.penup()
-        self.wall_turtle.goto(self.LEFT_WALL, self.TOP_WALL)
+        self.wall_turtle.goto(-WIDTH / 2 * self.square_size, HEIGHT / 2 * self.square_size)
         self.wall_turtle.pendown()
 
-        self.wall_turtle.forward(10 * square_size)
+        self.wall_turtle.forward(WIDTH * self.square_size)
         self.wall_turtle.setheading(270)
-        self.wall_turtle.forward(20 * square_size)
+        self.wall_turtle.forward(HEIGHT * self.square_size)
         self.wall_turtle.setheading(180)
-        self.wall_turtle.forward(10 * square_size)
+        self.wall_turtle.forward(WIDTH * self.square_size)
         self.wall_turtle.setheading(90)
-        self.wall_turtle.forward(20 * square_size)
+        self.wall_turtle.forward(HEIGHT * self.square_size)
         self.wall_turtle.setheading(0)
