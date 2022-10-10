855494ead0bbe226221560e0f14592d119b2a7d5
Alex Quin <alex.j.quin@outlook.com>
2022-10-10 15:25:36 -0700

    Enable move left and right

--- main.py
+++ main.py
@@ -9,40 +9,8 @@ from game_board import GameBoard
 from piece_manager import PieceManager
 from scoreboard import Scoreboard
 
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 800
-SQUARE_SIZE = 30
 
-screen = Screen()
-screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
-screen.title("Turtle Tetris")
-screen.bgpic("resources/beach.gif")
-screen.tracer(0)
-screen.listen()
-
-game_board = GameBoard(SQUARE_SIZE)
-
-# TODO: Build piece manager
-piece_manager = PieceManager()
-
-scoreboard = Scoreboard()
-
-canvas = turtle.getcanvas()
-# canvas.bind("<Left>", piece_manager.move_left)
-canvas.bind("<Down>", piece_manager.move_down)
-# canvas.bind("<Right>", piece_manager.move_right)
-# canvas.bind("<Control_L>", piece_manager.swap_hold_piece)
-
-screen.update()
-
-global game_is_on
-game_is_on = True
-
-global delay
-delay = 1
-
-
-def move_down_event():
+def move_down_event(event=None):
     global delay
     global game_is_on
 
@@ -60,17 +28,40 @@ def move_down_event():
     turtle.ontimer(move_down_event, t=delay * 1000)
 
 
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 800
+SQUARE_SIZE = 30
+
+screen = Screen()
+screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+screen.title("Turtle Tetris")
+screen.bgpic("resources/beach.gif")
+screen.tracer(0)
+screen.listen()
+
+game_board = GameBoard(SQUARE_SIZE)
+
+piece_manager = PieceManager(SQUARE_SIZE)
+
+scoreboard = Scoreboard()
+
+canvas = turtle.getcanvas()
+canvas.bind("<Left>", piece_manager.move_left)
+canvas.bind("<Down>", move_down_event)
+canvas.bind("<Right>", piece_manager.move_right)
+# canvas.bind("<Control_L>", piece_manager.swap_hold_piece)
+
+screen.update()
+
+global game_is_on
+game_is_on = True
+
+global delay
+delay = 1
+
 turtle.ontimer(move_down_event, t=delay * 1000)
 
 while game_is_on:
-    # if not piece_manager.move_down():
-    #     pass
-    #         # num_lines = game_board.check_lines()
-    #         # scoreboard.add(num_lines)
-    #         # piece_manager.get_next_piece()
-    #
-    #
-    #
     screen.update()
 
 # scoreboard.game_over()
