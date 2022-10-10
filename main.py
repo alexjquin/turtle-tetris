ed26c80e27db7a261bb7d17fab63b097b2c3c347
Alex Quin <alex.j.quin@outlook.com>
2022-10-10 14:58:00 -0700

    Scoreboard done

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import tkinter as tk
 
 from game_board import GameBoard
 from piece_manager import PieceManager
+from scoreboard import Scoreboard
 
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 800
 SQUARE_SIZE = 30
 
@@ -24,8 +25,7 @@ game_board = GameBoard(SQUARE_SIZE)
 # TODO: Build piece manager
 piece_manager = PieceManager()
 
-# TODO: Build Scoreboard
-# scoreboard = Scoreboard()
+scoreboard = Scoreboard()
 
 canvas = turtle.getcanvas()
 # canvas.bind("<Left>", piece_manager.move_left)
@@ -35,28 +35,43 @@ canvas.bind("<Down>", piece_manager.move_down)
 
 screen.update()
 
+global game_is_on
 game_is_on = True
+
+global delay
 delay = 1
 
+
 def move_down_event():
+    global delay
+    global game_is_on
+
     if not piece_manager.move_down():
-        # num_lines = piece_manager.check_lines()
-        # scoreboard.add(num_lines)
-        piece_manager.get_next_piece()
+        if piece_manager.is_full():
+            game_is_on = False
+        else:
+            num_lines = piece_manager.check_lines()
+            scoreboard.add(num_lines)
+            piece_manager.next_piece()
+
+            if scoreboard.lines > 0 and scoreboard.lines % 10:
+                scoreboard.level_up()
+                delay *= 0.9
     turtle.ontimer(move_down_event, t=delay * 1000)
+
 
 turtle.ontimer(move_down_event, t=delay * 1000)
 
 while game_is_on:
     # if not piece_manager.move_down():
     #     pass
-#         # num_lines = game_board.check_lines()
-#         # scoreboard.add(num_lines)
-#         # piece_manager.get_next_piece()
-#
-#
-#
+    #         # num_lines = game_board.check_lines()
+    #         # scoreboard.add(num_lines)
+    #         # piece_manager.get_next_piece()
+    #
+    #
+    #
     screen.update()
 
-
+# scoreboard.game_over()
 screen.mainloop()
