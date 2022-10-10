266de1d73266b2f2250df8678cb0214ce1a3dce1
Alex Quin <alex.j.quin@outlook.com>
2022-10-10 14:23:38 -0700

    move_down on timer and keypress, draw_queue, draw_piece, queue_new done

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import threading
 import time
 import turtle
 from turtle import Screen
 
-import game_board
+import tkinter as tk
+
+from game_board import GameBoard
 from piece_manager import PieceManager
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
-
-speed = 1
+SQUARE_SIZE = 30
 
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
@@ -17,30 +19,44 @@ screen.bgpic("resources/beach.gif")
 screen.tracer(0)
 screen.listen()
 
-# TODO: Draw board
-board.draw_board()
+game_board = GameBoard(SQUARE_SIZE)
 
 # TODO: Build piece manager
 piece_manager = PieceManager()
 
 # TODO: Build Scoreboard
+# scoreboard = Scoreboard()
 
 canvas = turtle.getcanvas()
-canvas.bind("<Left>", piece_manager.move_left)
+# canvas.bind("<Left>", piece_manager.move_left)
 canvas.bind("<Down>", piece_manager.move_down)
-canvas.bind("<Right>", piece_manager.move_right)
-canvas.bind("<Control_L>", piece_manager.swap_hold_piece)
+# canvas.bind("<Right>", piece_manager.move_right)
+# canvas.bind("<Control_L>", piece_manager.swap_hold_piece)
 
 screen.update()
 
 game_is_on = True
+delay = 1
+
+def move_down_event():
+    if not piece_manager.move_down():
+        # num_lines = piece_manager.check_lines()
+        # scoreboard.add(num_lines)
+        piece_manager.get_next_piece()
+    turtle.ontimer(move_down_event, t=delay * 1000)
+
+turtle.ontimer(move_down_event, t=delay * 1000)
 
 while game_is_on:
-    if not piece_manager.move_down():
-        piece_manager.next_piece()
-
+    # if not piece_manager.move_down():
+    #     pass
+#         # num_lines = game_board.check_lines()
+#         # scoreboard.add(num_lines)
+#         # piece_manager.get_next_piece()
+#
+#
+#
     screen.update()
 
-# 10*20 squares
 
 screen.mainloop()
--- piece_manager.py
+++ piece_manager.py
@@ -1,65 +1,154 @@
+import random
 from turtle import Turtle
 import game_board
 
+PIECES = ["O", "I", "S", "Z", "L", "J", "T"]
+O = [(-1, 0), (-1, -1), (0, 0), (0,-1)]
+I = [(-1, 0), (0, 0), (1, 0), (2, 0)]
+S = [(0, 0), (1, 0), (-1, -1), (0, -1)]
+Z = [(-1, 0), (0, 0), (0, -1), (1, -1)]
+L = [(-1, -1), (0, -1), (1, -1), (1, 0)]
+J = [(-1, 0), (-1, -1), (0, -1), (1, -1)]
+T = [(-1, -1), (0, 0), (0, -1), (1, -1)]
+
+O_COLOR = "#edf200"
+I_COLOR = "#00d8f2"
+S_COLOR = "#ea0000"
+Z_COLOR = "#64ad23"
+L_COLOR = "#f28600"
+J_COLOR = "#f24db2"
+T_COLOR = "#97008e"
+
+ORIGIN = (15, 285)
+
+QUEUE_ORIGINS = [(215, 285), (215, 195), (215, 105)]
+
+
 class PieceManager:
     def __init__(self):
+        self.board = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+
         self.active_piece = None
         self.queue = []
         self.hold_piece = []
 
-    def swap_hold_piece(self):
-        if self.hold_piece is None:
-            self.hold_piece = self.active_piece
-            self.active_piece = self.queue[0]
-
-            self.queue.remove(self.active_piece)
-
+        for index in range(0, 3):
             self.queue_new()
-        else:
-            temp = self.active_piece
-            self.active_piece = self.hold_piece
-            self.hold_piece = temp
 
+        self.next_piece()
+
+        self.draw_queue()
+
+        self.active_piece.draw_piece(ORIGIN)
+
+    # def swap_hold_piece(self):
+    #     if self.hold_piece is None:
+    #         self.hold_piece = self.active_piece
+    #         self.active_piece = self.queue[0]
+    #
+    #         self.queue.remove(self.active_piece)
+    #
+    #         self.queue_new()
+    #     else:
+    #         temp = self.active_piece
+    #         self.active_piece = self.hold_piece
+    #         self.hold_piece = temp
+    #         # TODO: put active piece at top
+    #
     def queue_new(self):
         # pick random new piece
         # add to end of queue
-        pass
+        new_shape = random.choice(PIECES)
+        new_piece = Piece(new_shape)
+        self.queue.append(new_piece)
+
+    def draw_queue(self):
+        for index, piece in enumerate(self.queue):
+            piece.draw_piece(QUEUE_ORIGINS[index])
 
     def next_piece(self):
         self.active_piece = self.queue[0]
+        self.queue.remove(self.active_piece)
         self.queue_new()
 
-class Piece:
-    def __init__(self):
-        self.squares = []
-
-    def move_left(self, left_wall):
-        for square in self.squares:
+    #
+    # def move_left(self, left_wall):
+    #     for square in self.active_piece.squares:
+    #         pass
+    #         # if cant move left:
+    #         #     return
+    #     for square in self.active_piece.squares:
+    #         x = square.xcor()
+    #         y = square.ycor()
+    #         square.goto(x - 20, y)
+    #
+    # def move_right(self):
+    #     for square in self.active_piece.squares:
+    #         pass
+    #     # if cant move right:
+    #     #     return
+    #     for square in self.active_piece.squares:
+    #         x = square.xcor()
+    #         y = square.ycor()
+    #         square.goto(x + 20, y)
+    #
+    def move_down(self, event=None):
+        for square in self.active_piece.turtles:
             pass
-            # if cant move left:
-            #     return
-        for square in self.squares:
-            x = square.xcor()
-            y = square.ycor()
-            square.goto(x - 20, y)
-
-    def move_right(self):
-        for square in self.squares:
-            pass
-        # if cant move right:
-        #     return
-        for square in self.squares:
-            x = square.xcor()
-            y = square.ycor()
-            square.goto(x + 20, y)
-
-    def move_down(self):
-        for square in self.squares:
-            pass
+            # check if cant move down
         # if cant move down:
         #     return False
-        for square in self.squares:
+        for square in self.active_piece.turtles:
             x = square.xcor()
             y = square.ycor()
-            square.goto(x + 20, y)
+            square.goto(x, y - 30)
         return True
+
+
+def new_turtle(color):
+    tim = Turtle()
+    tim.shape("turtle")
+    tim.color("black", color)
+    tim.penup()
+    tim.setheading(270)
+    tim.shapesize(1.4)
+
+    return tim
+
+
+class Piece:
+    def __init__(self, new_shape):
+        self.turtles = []
+
+        if new_shape == "O":
+            self.make_piece(O_COLOR)
+            self.shape = O
+        elif new_shape == "I":
+            self.make_piece(I_COLOR)
+            self.shape = I
+        elif new_shape == "S":
+            self.make_piece(S_COLOR)
+            self.shape = S
+        elif new_shape == "Z":
+            self.make_piece(Z_COLOR)
+            self.shape = Z
+        elif new_shape == "L":
+            self.make_piece(L_COLOR)
+            self.shape = L
+        elif new_shape == "J":
+            self.make_piece(J_COLOR)
+            self.shape = J
+        elif new_shape == "T":
+            self.make_piece(T_COLOR)
+            self.shape = T
+
+    def make_piece(self, color: str):
+        for index in range(0, 4):
+            tim = new_turtle(color)
+            self.turtles.append(tim)
+
+    def draw_piece(self, origin):
+        for index, coordinates in enumerate(self.shape):
+            self.turtles[index].goto(coordinates[0] * 30 + origin[0], coordinates[1] * 30 + origin[1])
+
+    # rotate around square[1]
