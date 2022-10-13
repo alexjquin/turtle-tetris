c0ea5a936aaaeddecda7588ec7b31e6db2d40437
Alex Quin <alex.j.quin@outlook.com>
2022-10-13 12:38:47 -0700

    Debug clear lines

--- piece_manager.py
+++ piece_manager.py
@@ -8,7 +8,7 @@ QUEUE_ORIGINS = [(215, 285), (215, 195), (215, 105)]
 ORIGIN_COORDINATES = (15, 285)
 ORIGIN_INDEX = (5, 19)
 
-HOLD_COORDINATES = (-215, 285)
+HOLD_COORDINATES = (-245, 285)
 
 
 class PieceManager:
@@ -47,6 +47,8 @@ class PieceManager:
             self.board.append(row)
 
     def swap_hold_piece(self, event=None):
+        self.active_piece.rotation = 1
+
         if self.hold_piece is None:
             self.hold_piece = self.active_piece
             self.hold_piece.draw_piece(HOLD_COORDINATES)
@@ -115,7 +117,26 @@ class PieceManager:
         return True
 
     def check_lines(self):
-        return 0
+        lines = 0
+        full_rows = []
+
+        for row in self.board:
+            full_row = True
+
+            for col in row:
+                if col is None:
+                    full_row = False
+
+            if full_row:
+                lines += 1
+                full_rows.append(row)
+                # self.board.remove(row)
+                # self.board.append([None, None, None, None, None, None, None, None, None, None])
+
+                for turt in row:
+                    turt.setheading(180)
+
+        return lines, full_rows
 
     def is_full(self):
         for space in self.board[19]:
@@ -214,34 +235,26 @@ class PieceManager:
 
     def rotate_left(self, event=None):
         if self.active_piece.rotation == 1:
-            print("Rotation == 1, Left")
             if self.active_piece.shape_name == "I":
                 if not self.rotation(add, sub, sub, add, sub, add, 2, 2):
-                    print("bad I rotation")
                     return
             elif self.active_piece.shape_name == "S":
                 if not self.rotation(sub, add, add, add, add, add, 2, 0):
-                    print("bad S rotation")
                     return
             elif self.active_piece.shape_name == "Z":
                 if not self.rotation(add, sub, add, add, add, add, 0, 2):
-                    print("bad Z rotation")
                     return
             elif self.active_piece.shape_name == "L":
                 if not self.rotation(add, sub, sub, add, sub, sub, 2, 0):
-                    print("bad L rotation")
                     return
             elif self.active_piece.shape_name == "J":
                 if not self.rotation(sub, add, add, sub, sub, sub, 0, 2):
-                    print("bad J rotation")
                     return
             elif self.active_piece.shape_name == "T":
                 if not self.rotation(add, sub, sub, sub, sub, add, 1, 1):
-                    print("bad T rotation")
                     return
 
         if self.active_piece.rotation == 2:
-            print("Rotation == 2, Left")
             if self.active_piece.shape_name == "I":
                 if not self.rotation(add, add, sub, sub, sub, sub, 2, 2):
                     return
@@ -262,7 +275,6 @@ class PieceManager:
                     return
 
         elif self.active_piece.rotation == 3:
-            print("Rotation == 3, Left")
             if self.active_piece.shape_name == "I":
                 if not self.rotation(sub, add, add, sub, add, sub, 2, 2):
                     return
@@ -283,7 +295,6 @@ class PieceManager:
                     return
 
         elif self.active_piece.rotation == 4:
-            print("Rotation == 4, Left")
             if self.active_piece.shape_name == "I":
                 if not self.rotation(sub, sub, add, add, add, add, 2, 2):
                     return
@@ -314,28 +325,22 @@ class PieceManager:
 
         turt_0_x = turt_0.x_index
         turt_0_y = turt_0.y_index
-
         # turtles[1] is rotation center and doesnt move
-
         turt_2_x = turt_2.x_index
         turt_2_y = turt_2.y_index
-
         turt_3_x = turt_3.x_index
         turt_3_y = turt_3.y_index
 
         turt_0_new_x = x_0_op(turt_0_x, 1)
         turt_0_new_y = y_0_op(turt_0_y, 1)
-        if not self.is_valid_rotation(turt_0_new_x, turt_0_new_y):
-            return False
-
         turt_2_new_x = x_2_op(turt_2_x, 1)
         turt_2_new_y = y_2_op(turt_2_y, 1)
-        if not self.is_valid_rotation(turt_2_new_x, turt_2_new_y):
-            return False
-
         turt_3_new_x = x_3_op(turt_3_x, x_3_mod)
         turt_3_new_y = y_3_op(turt_3_y, y_3_mod)
-        if not self.is_valid_rotation(turt_3_new_x, turt_3_new_y):
+        if not self.is_valid_rotation(turt_0_new_x, turt_0_new_y) or \
+                not self.is_valid_rotation(turt_2_new_x, turt_2_new_y) or \
+                not self.is_valid_rotation(turt_3_new_x, turt_3_new_y):
+            #TODO: Try shifting up, left, right, or down to make rotation work
             return False
 
         turt_0.x_index = turt_0_new_x
@@ -348,15 +353,30 @@ class PieceManager:
 
         turt_3.x_index = turt_3_new_x
         turt_3.y_index = turt_3_new_y
-        turt_3.goto(x_3_op(turt_3.xcor(), x_3_mod * self.square_size), y_3_op(turt_3.ycor(), y_3_mod * self.square_size))
+        turt_3.goto(x_3_op(turt_3.xcor(), x_3_mod * self.square_size),
+                    y_3_op(turt_3.ycor(), y_3_mod * self.square_size))
 
         return True
 
     def is_valid_rotation(self, x, y):
         return not out_of_bounds(x, y) and self.board[y][x] is None
 
+    def shift_pieces_down(self, full_rows):
+        index = 0
+        while index < 20:
+            row = self.board[index]
+            if row in full_rows:
+                self.board.remove(row)
+                self.board.append([None, None, None, None, None, None, None, None, None, None])
+                # shift all rows above down 1
+                for row_above in self.board[index:]:
+                    for turt in row_above:
+                        if turt is not None:
+                            turt.forward(self.square_size)
+                            turt.y_index -= 1
+                index -= 1
+            index += 1
+
 
 def out_of_bounds(x, y):
     return x < 0 or x > 9 or y < 0 or y > 19
-
-
